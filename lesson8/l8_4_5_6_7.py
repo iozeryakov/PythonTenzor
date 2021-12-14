9af6925b8d4e89805694e47dbff1a52a30479903
iozeryakov <94650470+iozeryakov@users.noreply.github.com>
2021-12-14 23:08:50 +0200

    Add files via upload

---
+++ lesson8/l8_4_5_6_7.py
@@ -0,0 +1,34 @@
+import time
+'''Задача 5'''
+def decorator1(func):
+    '''Декоратор: логирование выполнения функции'''
+    def wapper(a,b):
+        print('Сумма',a,'и',b,'равна:',end=' ')
+        func(a,b)
+    return wapper
+'''Задача 6'''
+def decorator2(func):
+    '''Декоратор: время выполнения функции'''
+    def wapper(a,b):
+        start_time=time.clock()
+        func(a,b)
+        print('Время выполнение функции:','%.5f' % (time.clock()-start_time),'секунд.')
+    return wapper
+'''Задача 7'''
+def decorator3(times):
+    '''Декоратор: замедление выполнения кода'''
+    def decorator(func):
+        def wapper(a,b):
+            time.sleep(times)
+            func(a,b)
+            print('Выполнение функции было замедлено на', times,'секунды.')
+        return wapper
+    return decorator
+@decorator2
+@decorator3(2)
+@decorator2
+@decorator1
+def sum(a,b):
+    '''Функция возвращает сумму 2-х чисел'''
+    print(a+b)
+sum(5,6)
